14c5294d2da1a5c1ecdae8fdf1b3605c283a50c9
aTule-csc <113103385+aTule-csc@users.noreply.github.com>
2024-12-19 17:40:59 +0500

    basic stuff ready (onItemClick by items need to be done next)

--- app/src/main/java/com/example/shoppinglist/Items.java
+++ app/src/main/java/com/example/shoppinglist/Items.java
@@ -8,17 +8,76 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Items extends AppCompatActivity {
+
+    private DBHelper dbHelper;
+    private SQLiteDatabase database;
+    private ListView listView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_items);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        dbHelper=new DBHelper(getApplicationContext());
+        try {
+            database=dbHelper.getWritableDatabase();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        listView=findViewById(R.id.ListView);
+        textView =findViewById(R.id.textView2);
+
+        Intent mIntent = getIntent();
+        int intValue = mIntent.getIntExtra("position", 0) + 1;
+        textView.setText(mIntent.getStringExtra("list_type"));
+
+
+        ArrayList<HashMap<String,String>> items =new ArrayList<>();
+        HashMap <String,String> item;
+        Cursor cursor = database.rawQuery("SELECT item_id, item_string, item_amount FROM item_in_list WHERE list_id = ?", new String[] { String.valueOf(intValue) } );
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()){
+            item=new HashMap<>();
+            item.put("name", cursor.getString(0));
+            item.put("info", cursor.getString(1) );
+            items.add(item);
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                getApplicationContext(),
+                items, android.R.layout.simple_list_item_2,
+                new String[]{"name","info"},
+                new int[]{android.R.id.text1, android.R.id.text2}
+        );
+        listView.setAdapter(adapter);
+
+    }
+    public void toMain(View v)
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.shoppinglist;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity{
+
+    private DBHelper dbHelper;
+    private SQLiteDatabase database;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,46 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        dbHelper=new DBHelper(getApplicationContext());
+        try {
+            database=dbHelper.getWritableDatabase();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        listView=findViewById(R.id.ListView);
+
+        ArrayList<HashMap<String,String>>types =new ArrayList<>();
+        HashMap <String,String> type;
+        Cursor cursor = database.rawQuery("SELECT list.id, list.name FROM list", null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()){
+            type=new HashMap<>();
+            type.put("list", cursor.getString(1));
+            types.add(type);
+            cursor.moveToNext();
+        }
+        cursor.close();
+
+        SimpleAdapter adapter = new SimpleAdapter(
+                getApplicationContext(),
+                types, android.R.layout.simple_list_item_2,
+                new String[]{"list"},
+                new int[]{android.R.id.text1}
+        );
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(getApplicationContext(), Items.class);
+                intent.putExtra("position",position);
+                HashMap itemData = (HashMap) listView.getItemAtPosition(position);
+                String armor_name = String.valueOf(itemData.get("list"));
+                intent.putExtra("list_type",armor_name);
+                startActivity(intent);
+            }
         });
     }
 }
